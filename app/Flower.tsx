bb16ba2bb7e31da580dbfcfd88b2cb6e9c536997
kennethsim2000 <e0725254@u.nus.edu>
2023-12-23 15:46:41 +0800

    added music

---
+++ app/Flower.tsx
@@ -0,0 +1,5 @@
+const Flower = () => {
+  return <div></div>;
+};
+
+export default Flower;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
-import Image from 'next/image'
-import Slider from './Slider.tsx';
+"use client";
+import Slider from "./Slider.tsx";
+import { useState } from "react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
-const animation = { duration: 5000, easing: (t:any) => t }
 export default function Home() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlay = () => {
+    setIsPlaying(true);
+  };
   return (
     <main className="flex h-screen flex-col items-center md:p-24 p-6">
-      <Slider/>
+      <Slider />
+
+      <ButtonGroup gap="4" className="mt-4">
+        <Button colorScheme="blackAlpha" onClick={handlePlay}>
+          Play
+        </Button>
+        <Button colorScheme="gray">Surprise</Button>
+      </ButtonGroup>
+      {/* <button onClick={handlePlay}>Play</button> */}
+      {isPlaying && (
+        <audio autoPlay loop>
+          <source src="/videos/Famy.mp3" type="audio/mp3" />
+          Your browser does not support the audio tag.
+        </audio>
+      )}
     </main>
-  )
+  );
 }
